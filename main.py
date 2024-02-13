209e29d611e2325a77dec3ec0a6868e97d85cda0
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-02-13 18:28:21 +0300

    The software now recognizes French words from a CSV file and outputs them.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+data_dict = data.to_dict(orient="records")
+
+def create_new_card():
+    rnd = random.choice(data_dict)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=rnd["French"])
+
+
 
 window = Tk()
 window.title("Flash Card App")
@@ -12,21 +24,18 @@ canvas.create_image((400, 263), image=card_front_img)
 canvas.config(background=BACKGROUND_COLOR)
 canvas.grid(row=0, column=0, columnspan=3, rowspan=8)
 
-title = Label(text="Title", font=("italic", 12), background=BACKGROUND_COLOR)
-title.grid(row=2, column=1)
-
-word = Label(text="Title", font=("italic", 12), background=BACKGROUND_COLOR)
-word.grid(row=4, column=1)
-
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 22, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 22, "bold"))
 
 button_red_img = PhotoImage(file="images/wrong.png")
-button_red = Button(image=button_red_img, highlightthickness=False)
+button_red = Button(image=button_red_img, command=create_new_card)
 button_red.grid(row=8, column=0)
 
 button_green_img = PhotoImage(file="images/right.png")
-button_green = Button(image=button_green_img, highlightthickness=False)
+button_green = Button(image=button_green_img)
 button_green.grid(row=8, column=2)
 
+create_new_card()
 
 
 window.mainloop()
